9afb4edaa5daa333e5c19c0fdec4ff1009e4a7d8
fabio <fabio@fabio-desktop>
2013-05-28 15:48:32 -0300

    Menu vqmod module and import controller

---
+++ admin/controller/squidfacil/import.php
@@ -0,0 +1,14 @@
+<?php 
+class ControllerModuleSquidfacilImport extends Controller {
+	private $error = array(); 
+     
+  	public function index() {
+
+            $this->document->setTitle("test"); 
+
+		
+		$this->getList();
+  	}
+  
+}
+?>
