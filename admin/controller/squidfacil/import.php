2a99bab464597f7d75e40fbf3e42ef009eccf118
fabio <fabio@fabio-desktop>
2013-05-29 10:31:05 -0300

    Module refactoring

---
+++ admin/controller/squidfacil/import.php
@@ -0,0 +1,115 @@
+<?php
+
+class ControllerSquidfacilImport extends Controller {
+
+    public function index() {
+        $this->load->language('squidfacil/import');
+        $this->document->setTitle("test");
+        $this->getList();
+    }
+
+    protected function getList() {
+		if (isset($this->request->get['page'])) {
+			$page = $this->request->get['page'];
+		} else {
+			$page = 1;
+		}
+		
+		$url = '';
+		
+		if (isset($this->request->get['page'])) {
+			$url .= '&page=' . $this->request->get['page'];
+		}
+						
+   		$this->data['breadcrumbs'] = array();
+
+   		$this->data['breadcrumbs'][] = array(
+       		'text'      => $this->language->get('text_home'),
+			'href'      => $this->url->link('common/home', 'token=' . $this->session->data['token'], 'SSL'),
+      		'separator' => false
+   		);
+
+   		$this->data['breadcrumbs'][] = array(
+       		'text'      => $this->language->get('heading_title'),
+			'href'      => $this->url->link('catalog/category', 'token=' . $this->session->data['token'] . $url, 'SSL'),
+      		'separator' => ' :: '
+   		);
+									
+		$this->data['import'] = $this->url->link('catalog/category/insert', 'token=' . $this->session->data['token'] . $url, 'SSL');
+		
+		$this->data['categories'] = array();
+		
+		$data = array(
+			'start' => ($page - 1) * $this->config->get('config_admin_limit'),
+			'limit' => $this->config->get('config_admin_limit')
+		);
+				
+		$category_total = 5; // $this->model_catalog_category->getTotalCategories();
+		/*
+		$results = $this->model_catalog_category->getCategories($data);
+
+		foreach ($results as $result) {
+			$action = array();
+						
+			$action[] = array(
+				'text' => $this->language->get('text_edit'),
+				'href' => $this->url->link('catalog/category/update', 'token=' . $this->session->data['token'] . '&category_id=' . $result['category_id'] . $url, 'SSL')
+			);
+
+			$this->data['categories'][] = array(
+				'category_id' => $result['category_id'],
+				'name'        => $result['name'],
+				'sort_order'  => $result['sort_order'],
+				'selected'    => isset($this->request->post['selected']) && in_array($result['category_id'], $this->request->post['selected']),
+				'action'      => $action
+			);
+		}
+		
+                 * 
+                 */
+		$this->data['heading_title'] = $this->language->get('heading_title');
+
+		$this->data['text_no_results'] = $this->language->get('text_no_results');
+
+		$this->data['column_name'] = $this->language->get('column_name');
+		$this->data['column_sort_order'] = $this->language->get('column_sort_order');
+		$this->data['column_action'] = $this->language->get('column_action');
+
+		$this->data['button_import'] = $this->language->get('button_import');
+
+ 
+ 		if (isset($this->error['warning'])) {
+			$this->data['error_warning'] = $this->error['warning'];
+		} else {
+			$this->data['error_warning'] = '';
+		}
+
+		if (isset($this->session->data['success'])) {
+			$this->data['success'] = $this->session->data['success'];
+		
+			unset($this->session->data['success']);
+		} else {
+			$this->data['success'] = '';
+		}
+		
+		$pagination = new Pagination();
+		$pagination->total = $category_total;
+		$pagination->page = $page;
+		$pagination->limit = $this->config->get('config_admin_limit');
+		$pagination->text = $this->language->get('text_pagination');
+		$pagination->url = $this->url->link('catalog/category', 'token=' . $this->session->data['token'] . $url . '&page={page}', 'SSL');
+			
+		$this->data['pagination'] = $pagination->render();
+		
+		$this->template = 'squidfacil/import.tpl';
+		$this->children = array(
+			'common/header',
+			'common/footer'
+		);
+				
+		$this->response->setOutput($this->render());
+	}
+
+}
+
+?>
---
+++ admin/language/english/squidfacil/import.php
@@ -0,0 +1,9 @@
+<?php
+$_['heading_title']       = 'Squid Fácil';
+
+$_['button_import']       = 'Import Products';
+
+$_['column_name']       = 'Product Name';
+$_['column_sort_order']       = 'Sort Order';
+$_['column_action']       = 'Action';
+?>
